ed0c0194c3da6577148343b4a66675e33f10d3a2
Waves59 <thibault.demars59@gmail.com>
2018-04-10 15:41:42 +0200

    Front = Navbar, Equipe, Carroussel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,21 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+
+
+// Automatic Slideshow - change image every 4 seconds
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 4000);    
+}
