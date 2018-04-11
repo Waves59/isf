d81f5700d6487297673e947c213b2ed00f174893
Waves59 <thibault.demars59@gmail.com>
2018-04-11 15:20:58 +0200

    ajout des categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410220013) do
+ActiveRecord::Schema.define(version: 20180411131856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "intuitors", force: :cascade do |t|
     t.string "first_name"
