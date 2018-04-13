481c13d7f1d8f5a4ca3bdc2bc73e2bb577a3f8c8
Waves59 <thibault.demars59@gmail.com>
2018-04-13 13:05:17 +0200

    installation de devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :categories
   resources :intuitors
   resources "contacts", only: [:new, :create]
+  devise_for :users,
+               :path => '', 
+               :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}
 
   root 'static#index'
   get '/about' => 'static#about'
