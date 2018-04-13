b10369d027c9355333204afe8527bb2adc1645ea
Waves59 <thibault.demars59@gmail.com>
2018-04-13 14:38:30 +0200

    réparation url

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   resources :categories
   resources :intuitors
   resources "contacts", only: [:new, :create]
-  devise_for :users
-  
+  devise_for :users,
+  :path => '', 
+  :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}
+
+
   root 'static#index'
   get '/about' => 'static#about'
 
